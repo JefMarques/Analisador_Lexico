fe709acd61b5aee1cda5431332c115f37ff4b58e
Jeferson <joedf1006@gmail.com>
2022-04-16 17:38:13 -0300

    confirmar

--- analiseLexica.py
+++ analiseLexica.py
@@ -1,42 +1,42 @@
-import re
-entrada = input("Digite um Lexema: ")
-lexema = entrada.replace(" ","")
-print("Antes do Regex:",lexema)
-lexema = re.sub(r'#.*','',lexema)
+import re   #Importando o biblioteca regex
+entrada = input("Digite um Lexema: ")   #Entrada do Lexema
+lexema = entrada.replace(" ","")    #Remover espaços caso tenha
+print("Antes do Regex:",lexema)     
+lexema = re.sub(r'#.*','',lexema)   #Remover linha de comentario caso tenha
 print("Depois do Regex:",lexema)
 
 
-tabelaDeSimbolo = []
+tabelaDeSimbolo = [] 
 tabelaLetra = []
 
 
-for i in range(len(lexema)):
+for i in range(len(lexema)):    #for para analisar o lexema
     
-    if lexema[i].isalpha():
-        tabelaLetra.append(lexema[i])
-        if lexema[i] in tabelaDeSimbolo:
-            del tabelaDeSimbolo[-1]
+    if lexema[i].isalpha():     #Analisa se cada item do lexema é letra
+        tabelaLetra.append(lexema[i])   #Se for letra adiciona na lista
+        if lexema[i] in tabelaDeSimbolo:    #Se for uma letra repetida 
+            del tabelaDeSimbolo[-1]     #Deleta uma posição na lista
 
-        tabelaDeSimbolo.append(lexema[i])
-        indexTabela = tabelaDeSimbolo.index(lexema[i])
+        tabelaDeSimbolo.append(lexema[i])   #Se for letra adiciona na lista
+        indexTabela = tabelaDeSimbolo.index(lexema[i])  #O index de cada letra na lista
 
-        print('<id',indexTabela+1,'> ', end='')
+        print('<id',indexTabela+1,'> ', end='') 
         
         
-    elif lexema[i].isdigit():
+    elif lexema[i].isdigit():   #Analisa se cada item do lexema é digito
         print('<digito', lexema[i]+'> ', end='')
     
-    elif lexema[i] in ['=', '+', '*']:
+    elif lexema[i] in ['=', '+', '*']:  #Analisa se o lexema tem esses operadores
         print('<'+lexema[i]+'> ', end='')
 
-    else:
+    else: #saida de erro caso o lexema possua algo fora do padrão
         print('<error> ', end='')
 
     
 
 print("")
-ct = 0
-tab = sorted(set(tabelaLetra))
-for v in (tab):
+ct = 0  #contador para a tabela de simbolos
+tab = sorted(set(tabelaLetra))  #Deixando a lista em ordem alfabetica e removendo itens repetidos
+for v in (tab): #rodando a lista
     ct+=1    
-    print(ct, v)
+    print(ct, v) 
